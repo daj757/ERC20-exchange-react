9b1765b3b3f3ea1e6d83e5d3943c842fa85e5cca
Danny Jimenez <daj777@gmail.com>
2018-07-02 18:14:05 -0600

    Delete keys.js

--- keys.js
+++
@@ -1,4 +0,0 @@
-export default keys = {
-    keyPhrase: "solar rapid vintage oval vendor evolve replace now exclude endorse grief stem",
-    infura: "https://rinkeby.infura.io/nJ4vwAo1AaL8XQT7p7XK"
-}
